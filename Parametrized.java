d2017e8158da55f71406606d2b9322f224d49e64
SnehaBaital <86301593+SnehaBaital@users.noreply.github.com>
2022-11-03 23:41:54 +0530

    commits

--- Parametrized.java
+++ Parametrized.java
@@ -4,10 +4,8 @@ public class Parametrized
 public static void main(String args[])  
 {  
 int x, y, sum;  
-Scanner sc = new Scanner(System.in);  
-System.out.print("Enter the first number: ");  
-x = sc.nextInt();  
-System.out.print("Enter the second number: ");  
+Scanner sc = new Scanner(System.in);    
+x = sc.nextInt();   
 y = sc.nextInt();  
 sum = sum(x, y);  
 System.out.println("The sum of two numbers x and y is: " + sum);  
